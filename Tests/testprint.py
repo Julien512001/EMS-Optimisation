697d58b09a0f048a95d21227bbd3329f7e39cbb2
Julien512001 <julien.moulis@student.uclouvain.be>
2025-08-18 09:06:18 +0200

    ok

--- Tests/testprint.py
+++
@@ -1,8 +0,0 @@
-DEBUG = True  # Passe à False pour désactiver
-
-def debug_print(*args, **kwargs):
-    if DEBUG:
-        print(*args, **kwargs)
-
-# Exemple d'utilisation
-debug_print("Ceci est un message de debug")
